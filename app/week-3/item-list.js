7204d0c48dd47c93e4318a1eb5613a518efb13b8
Usman Basharat <usmanbasharat1027@gmail.com>
2025-09-23 12:27:09 -0600

    optional updates: updated title page and used map

--- app/week-3/item-list.js
+++ app/week-3/item-list.js
@@ -1,91 +1,83 @@
 import Item from "./item.js";
 
 export default function ItemList() {
-  const item1 = {
-    name: "milk, 4 L 🥛",
-    quantity: 1,
-    category: "dairy",
-  };
+  const items = [
+    {
+      name: "milk, 4 L 🥛",
+      quantity: 1,
+      category: "dairy",
+    },
+    {
+      name: "bread 🍞",
+      quantity: 2,
+      category: "bakery",
+    },
 
-  const item2 = {
-    name: "bread 🍞",
-    quantity: 2,
-    category: "bakery",
-  };
+    {
+      name: "eggs, dozen 🥚",
+      quantity: 2,
+      category: "dairy",
+    },
 
-  const item3 = {
-    name: "eggs, dozen 🥚",
-    quantity: 2,
-    category: "dairy",
-  };
+    {
+      name: "bananas 🍌",
+      quantity: 6,
+      category: "produce",
+    },
 
-  const item4 = {
-    name: "bananas 🍌",
-    quantity: 6,
-    category: "produce",
-  };
+    {
+      name: "broccoli 🥦",
+      quantity: 3,
+      category: "produce",
+    },
 
-  const item5 = {
-    name: "broccoli 🥦",
-    quantity: 3,
-    category: "produce",
-  };
+    {
+      name: "chicken breasts, 1 kg 🍗",
+      quantity: 1,
+      category: "meat",
+    },
 
-  const item6 = {
-    name: "chicken breasts, 1 kg 🍗",
-    quantity: 1,
-    category: "meat",
-  };
+    {
+      name: "pasta sauce 🍝",
+      quantity: 3,
+      category: "canned goods",
+    },
 
-  const item7 = {
-    name: "pasta sauce 🍝",
-    quantity: 3,
-    category: "canned goods",
-  };
+    {
+      name: "spaghetti, 454 g 🍝",
+      quantity: 2,
+      category: "dry goods",
+    },
 
-  const item8 = {
-    name: "spaghetti, 454 g 🍝",
-    quantity: 2,
-    category: "dry goods",
-  };
+    {
+      name: "toilet paper, 12 pack 🧻",
+      quantity: 1,
+      category: "household",
+    },
 
-  const item9 = {
-    name: "toilet paper, 12 pack 🧻",
-    quantity: 1,
-    category: "household",
-  };
+    {
+      name: "paper towels, 6 pack",
+      quantity: 1,
+      category: "household",
+    },
 
-  const item10 = {
-    name: "paper towels, 6 pack",
-    quantity: 1,
-    category: "household",
-  };
+    {
+      name: "dish soap 🍽️",
+      quantity: 1,
+      category: "household",
+    },
 
-  const item11 = {
-    name: "dish soap 🍽️",
-    quantity: 1,
-    category: "household",
-  };
-
-  const item12 = {
-    name: "hand soap 🧼",
-    quantity: 4,
-    category: "household",
-  };
+    {
+      name: "hand soap 🧼",
+      quantity: 4,
+      category: "household",
+    },
+  ];
   return (
     <div>
-      <Item {...item1} />
-      <Item {...item2} />
-      <Item {...item3} />
-      <Item {...item4} />
-      <Item {...item5} />
-      <Item {...item6} />
-      <Item {...item7} />
-      <Item {...item8} />
-      <Item {...item9} />
-      <Item {...item10} />
-      <Item {...item11} />
-      <Item {...item12} />
+      {items.map((item) => (
+        <Item key={item.name} {...item} />
+      ))}
     </div>
   );
 }
