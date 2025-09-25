1e42d1c07755213e1702b737f4080d1124aae2db
Usman Basharat <usmanbasharat1027@gmail.com>
2025-09-25 10:45:07 -0600

    changed alignment to match teacher's output

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,6 +1,6 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <div className="flex border-1 border-white mb-2 mr-50 ml-50 p-2 rounded-sm">
+    <div className="flex border-1 border-white mb-2 mr-170 ml-170 p-2 rounded-sm">
       <ul>
         <li>{name}</li>
         <li className="capitalize">Quantity: {quantity}</li>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list.js";
 export default function Page() {
   return (
     <main>
-      <header className="flex ml-50 ">
+      <header className="flex ml-170 ">
         <h1 className="text-2xl font-bold my-3">Shopping List</h1>
       </header>
       <div>
