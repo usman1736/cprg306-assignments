69f128934ffa48b71b1972ca7dc98bc23a687543
Usman Basharat <usmanbasharat1027@gmail.com>
2025-10-10 10:46:33 -0600

    created name field, category field and a submit button

--- app/week-5/new-items.js
+++ app/week-5/new-items.js
@@ -14,6 +14,9 @@ export default function NewItems() {
       category: category,
     };
     console.log(item);
+    alert(`Item: ${name}
+      Quantity: ${quantity}
+      Category: ${category}`);
     setName("");
     setQuantity(1);
     setCategory("produce");
@@ -21,39 +24,73 @@ export default function NewItems() {
   return (
     <div>
       <header className="text-2xl flex justify-center mr-50 font-bold mt-6">
-        <h1>Add New Item</h1>
+        <h1>Week 5 — New Item</h1>
       </header>
       <section className="flex flex-row justify-center">
         <div className="ml-10 border-1 border-black bg-white text-gray-600 pt-6 pb-4 pl-4 mt-4 rounded-sm w-100 h-40">
-          <p>
-            Quantity: <span className="text-gray-300">{quantity}</span>
-          </p>
-          <div className="flex gap-4 mt-4 mb-4">
-            <div className="flex flex-row justify-center items-center bg-gray-300 p-4 rounded-sm w-10 h-10">
-              <button
-                className="text-gray-500"
-                onClick={() =>
-                  setQuantity(
-                    (quantity > 1) & (quantity <= 20) ? quantity - 1 : quantity
-                  )
-                }
-              >
-                -
-              </button>
+          <form onSubmit={(event) => handleSubmit(event)}>
+            <label htmlFor="name">Item Name</label>
+            <input
+              type="text"
+              required
+              id="name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              placeholder="eg., milk, 4 L 🥛"
+            />
+            <p>
+              Quantity: <span className="text-gray-300">{quantity}</span>
+            </p>
+            <div className="flex gap-4 mt-4 mb-4">
+              <div className="flex flex-row justify-center items-center bg-gray-300 p-4 rounded-sm w-10 h-10">
+                <button
+                  type="button"
+                  className="text-gray-500"
+                  onClick={() =>
+                    setQuantity(
+                      (quantity > 1) & (quantity <= 20)
+                        ? quantity - 1
+                        : quantity
+                    )
+                  }
+                >
+                  -
+                </button>
+              </div>
+              <div className="flex flex-row justify-center items-center bg-blue-400 rounded sm p-4 w-10 h-10 hover:bg-blue-500">
+                <button
+                  type="button"
+                  onClick={() =>
+                    setQuantity(
+                      quantity >= 1 && quantity < 20 ? quantity + 1 : quantity
+                    )
+                  }
+                >
+                  <span className="text-white">+</span>
+                </button>
+              </div>
             </div>
-            <div className="flex flex-row justify-center items-center bg-blue-400 rounded sm p-4 w-10 h-10 hover:bg-blue-500">
-              <button
-                onClick={() =>
-                  setQuantity(
-                    (quantity >= 1) & (quantity < 20) ? quantity + 1 : quantity
-                  )
-                }
-              >
-                <span className="text-white">+</span>
-              </button>
-            </div>
-          </div>
-          <p className="text-gray-500">Allowed range: 1-20</p>
+            <label htmlFor="category">Category</label>
+            <select
+              id="category"
+              onChange={(event) => setCategory(event.target.value)}
+              value={category}
+            >
+              <option value="produce">Produce</option>
+              <option value="diary">Dairy</option>
+              <option value="bakery">Bakery</option>
+              <option value="meat">Meat</option>
+              <option value="frozen foods">Frozen Foods</option>
+              <option value="canned goods">Canned Goods</option>
+              <option value="dry goods">Dry Goods</option>
+              <option value="beverages">Beverages</option>
+              <option value="snacks">Snacks</option>
+              <option value="household">Household</option>
+              <option value="other">Other</option>
+            </select>
+            <p className="text-gray-500">Allowed range: 1-20</p>
+            <button type="submit">Add Item</button>
+          </form>
         </div>
       </section>
     </div>
