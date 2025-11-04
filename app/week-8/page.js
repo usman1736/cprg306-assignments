5291d23fcfd8a9aff7c11de2bef21724b686ad14
Usman Basharat <usmanbasharat1027@gmail.com>
2025-11-04 10:02:22 -0700

    set up files

---
+++ app/week-8/page.js
@@ -0,0 +1,24 @@
+"use client";
+import ItemList from "./item-list";
+import NewItems from "./new-items";
+import itemData from "./items.json";
+import { useState } from "react";
+
+export default function Page() {
+  const [items, setItems] = useState([...itemData]);
+
+  function handleAddItem(item) {
+    setItems([...items, item]);
+  }
+  return (
+    <main>
+      <header className="flex ml-170 md:ml-80">
+        <h1 className="text-2xl font-bold my-3">Shopping List</h1>
+      </header>
+      <NewItems onAddItem={handleAddItem} />
+      <div>
+        <ItemList passingItems={items} />
+      </div>
+    </main>
+  );
+}
